b2fccd854c4443d1a6301281fa7374dd13203d8f
Christian Alvaro <christianchoques@gmail.com>
2024-05-13 02:16:19 -0500

    feat : Añadir NavBar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='w-[100%] bg-[#141c27] h-[10vh] shadow-md top-0'>
+            <div className='flex items-center justify-between w-[80%] mx-auto h-[100%]'>
+                <h1 className='text-white'>Investigación Operativa</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import Principal from "../pages/Principal"
+import Navbar from "../components/Navbar"
 
 const AppRouter = () => {
     return (
         <>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Navigate to="/home" />} />
-                <Route path="home" element={<Principal/>} />
-                
+                <Route path="home" element={<Principal />} />
+
             </Routes>
         </>
     )
